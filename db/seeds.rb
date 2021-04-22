0e6b1298ef9d822c0bd0f93938b98285eaee988d
Jamon Dixon <jamonadixon@gmail.com>
2021-04-22 17:22:08 -0600

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-quesos = Queso.create([
+Queso.create([
     {
         
         restaurant: "North County",
