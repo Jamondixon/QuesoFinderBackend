066e4405bdc6fd75fc861261db3de42f80c38565
Jamon Dixon <jamonadixon@gmail.com>
2021-04-22 17:36:51 -0600

    fix2

--- app/controllers/quesos_controller.rb
+++ app/controllers/quesos_controller.rb
@@ -1,2 +1,12 @@
 class QuesosController < ApplicationController
+    def index
+        @quesos = Queso.all
+        render json: @quesos
+    end
+
+    def show
+        @queso = Queso.find(params[:id])
+        render json: @queso
+    end
+
 end
