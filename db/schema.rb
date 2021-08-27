ac56e4ae40b973961c666ea9eede14ecca6b16f4
Jamon Dixon <jamonadixon@gmail.com>
2021-08-27 16:47:25 -0500

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_04_22_222606) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "quesos", force: :cascade do |t|
     t.string "restaurant"
     t.string "name"
